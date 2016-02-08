68f66b3d1deb6bfbf4019be27f1ab8feb40bf079
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-02-08 16:09:26 +0200

    Hello World!

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -91,4 +91,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionSay($message = 'Hello World!')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
 }
